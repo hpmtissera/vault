9045e4f044091dd1af4319a0429094d0bcf8c90c
Prasad Tissera <hpmtissera@gmail.com>
2018-12-11 10:16:43 +0900

    Add Postgresql db connection

--- src/main/kotlin/com/fandoco/vault/GreetingController.kt
+++ src/main/kotlin/com/fandoco/vault/GreetingController.kt
@@ -1,5 +1,11 @@
 package com.fandoco.vault
 
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -14,4 +20,39 @@ class GreetingController {
     fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
             Greeting(counter.incrementAndGet(), "Hello, $name")
 
+}
+
+fun main(args: Array<String>) {
+    Database.connect("jdbc:postgresql://localhost:5432/vault", driver = "org.postgresql.Driver",
+            user = "vault", password = "password")
+
+    transaction {
+        // print sql to std-out
+        addLogger(StdOutSqlLogger)
+
+        SchemaUtils.create (Cities)
+
+        // insert new city. SQL: INSERT INTO Cities (name) VALUES ('St. Petersburg')
+        val stPeteId = Cities.insert {
+            it[name] = "St. Petersburg"
+        } get Cities.id
+
+        // 'select *' SQL: SELECT Cities.id, Cities.name FROM Cities
+        City.all().forEach { println(it.name) }
+    }
+
+}
+
+object Cities: IntIdTable() {
+    val name = varchar("name", 50)
+}
+
+class City( id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<City>(Cities)
+
+    var name by Cities.name
+
+    override fun toString(): String {
+        return name
+    }
 }
