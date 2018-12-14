60c07761857c3ba430fcbe997c66ae5343bcd1a2
Prasad Tissera <hpmtissera@gmail.com>
2018-12-14 16:54:36 +0900

    refactor

--- src/main/kotlin/com/fandoco/vault/SecureDataRepository.kt
+++ src/main/kotlin/com/fandoco/vault/SecureDataRepository.kt
@@ -10,175 +10,182 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 fun main(args: Array<String>) {
-    Database.connect("jdbc:postgresql://localhost:5432/vault", driver = "org.postgresql.Driver",
-            user = "vault", password = "password")
 
-    transaction { SecureDataTable.deleteAll() }
-
-    val id1 = addEntry("Postbank", "Username", "Saman")
-    val id2 = addEntry("Postbank", "Password", "Kamal")
+    val id1 = SecureDataRepository.addEntry("Postbank", "Username", "Saman")
+    val id2 = SecureDataRepository.addEntry("Postbank", "Password", "Kamal")
 
     println("\nEntry added : $id1")
     println("\nEntry added : $id2")
 
-    printAll()
+    SecureDataRepository.printAll()
 
-    updateEntry(SecureDataEntry("Postbank", "Username", "Samankamal"))
+    SecureDataRepository.updateEntry(SecureDataEntry("Postbank", "Username", "Samankamal"))
 
-    printAll()
+    SecureDataRepository.printAll()
 
-    val entry = getEntry("Postbank", "Username")
+    val entry = SecureDataRepository.getEntry("Postbank", "Username")
 
     println("\n$entry\n")
 
-    val postBankEntries = getEntries("Postbank")
+    val postBankEntries = SecureDataRepository.getEntries("Postbank")
     println("\n$postBankEntries\n")
 
-    deleteEntry(SecureDataEntry("Postbank", "Username", null))
+    SecureDataRepository.deleteEntry(SecureDataEntry("Postbank", "Username", null))
 
-    printAll()
+    SecureDataRepository.printAll()
 
     println("UUID = $id2")
     println("UUID from String = ${UUID.fromString(id2)}")
-    deleteEntry(id2)
+    SecureDataRepository.deleteEntry(id2)
 
-    printAll()
+    SecureDataRepository.printAll()
 
 }
 
-fun printAll() {
-    val entries = getAllEntries()
-    when {
-        entries.isEmpty() -> println("\nTable is empty.")
-        else -> println(entries)
+object SecureDataRepository {
+
+    init {
+        Database.connect("jdbc:postgresql://localhost:5432/vault", driver = "org.postgresql.Driver",
+                user = "vault", password = "password")
     }
-}
 
-fun addEntry(type: String, key: String, value: String): String {
-    var id: String? = null
-    transaction {
-        // print sql to std-out
-        addLogger(StdOutSqlLogger)
-
-        val entityId = SecureDataTable.insert { row ->
-            row[SecureDataTable.type] = type
-            row[SecureDataTable.key] = key
-            row[SecureDataTable.value] = value
-        } get SecureDataTable.id
-
-        id = entityId?.value.toString()
-
+    fun deleteAll() {
+        transaction { SecureDataTable.deleteAll() }
     }
-    when (id) {
-        null -> throw Exception("Error while trying to insert entry. {type: $type, key: $key, value: $value")
-        else -> return id as String
-    }
-}
 
-fun getEntry(id: String): SecureDataEntry? {
-    var entry: SecureDataEntry? = null
-    transaction {
-        addLogger(StdOutSqlLogger)
-
-        val row = SecureDataTable.select { SecureDataTable.id eq UUID.fromString(id) }.singleOrNull()
-        if (row != null) {
-            entry = SecureDataEntry(
-                    row[SecureDataTable.id].toString(),
-                    row[SecureDataTable.type],
-                    row[SecureDataTable.key],
-                    row[SecureDataTable.value])
+    fun printAll() {
+        val entries = getAllEntries()
+        when {
+            entries.isEmpty() -> println("\nTable is empty.")
+            else -> println(entries)
         }
     }
-    return entry
-}
 
-fun getEntry(type: String, key: String): SecureDataEntry? {
-    var entry: SecureDataEntry? = null
-    transaction {
-        addLogger(StdOutSqlLogger)
+    fun addEntry(type: String, key: String, value: String): String {
+        var id: String? = null
+        transaction {
+            // print sql to std-out
+            addLogger(StdOutSqlLogger)
 
-        val row = SecureDataTable.select {
-            (SecureDataTable.type eq type) and (SecureDataTable.key eq key)
-        }.singleOrNull()
+            val entityId = SecureDataTable.insert { row ->
+                row[SecureDataTable.type] = type
+                row[SecureDataTable.key] = key
+                row[SecureDataTable.value] = value
+            } get SecureDataTable.id
 
-        if (row != null) {
-            entry = SecureDataEntry(
-                    row[SecureDataTable.id].toString(),
-                    row[SecureDataTable.type],
-                    row[SecureDataTable.key],
-                    row[SecureDataTable.value])
+            id = entityId?.value.toString()
+
+        }
+        when (id) {
+            null -> throw Exception("Error while trying to insert entry. {type: $type, key: $key, value: $value")
+            else -> return id as String
         }
     }
-    return entry
-}
 
-fun getEntries(type: String): List<SecureDataEntry> {
-    val entries = ArrayList<SecureDataEntry>()
-    transaction {
-        addLogger(StdOutSqlLogger)
+    fun getEntry(id: String): SecureDataEntry? {
+        var entry: SecureDataEntry? = null
+        transaction {
+            addLogger(StdOutSqlLogger)
 
-        SecureDataTable.select { SecureDataTable.type eq type }.forEach { row ->
-            entries.add(SecureDataEntry(
-                    row[SecureDataTable.id].toString(),
-                    row[SecureDataTable.type],
-                    row[SecureDataTable.key],
-                    row[SecureDataTable.value]))
+            val row = SecureDataTable.select { SecureDataTable.id eq UUID.fromString(id) }.singleOrNull()
+            if (row != null) {
+                entry = SecureDataEntry(
+                        row[SecureDataTable.id].toString(),
+                        row[SecureDataTable.type],
+                        row[SecureDataTable.key],
+                        row[SecureDataTable.value])
+            }
         }
-
+        return entry
     }
-    return entries
-}
 
-fun getAllEntries(): List<SecureDataEntry> {
-    val entries = ArrayList<SecureDataEntry>()
-    transaction {
-        SecureDataRow.all().forEach { entries.add(SecureDataEntry(it.id.toString(), it.type, it.key, it.value)) }
-    }
-    return entries
-}
+    fun getEntry(type: String, key: String): SecureDataEntry? {
+        var entry: SecureDataEntry? = null
+        transaction {
+            addLogger(StdOutSqlLogger)
 
-fun updateEntry(entry: SecureDataEntry) {
-    updateEntry(entry.type, entry.key, entry.value)
-}
+            val row = SecureDataTable.select {
+                (SecureDataTable.type eq type) and (SecureDataTable.key eq key)
+            }.singleOrNull()
 
-fun updateEntry(type: String, key: String, value: String?) {
-    transaction {
-        addLogger(StdOutSqlLogger)
-
-        SecureDataTable.update({ (SecureDataTable.type eq type) and (SecureDataTable.key eq key) })
-        {
-            it[SecureDataTable.value] = value
+            if (row != null) {
+                entry = SecureDataEntry(
+                        row[SecureDataTable.id].toString(),
+                        row[SecureDataTable.type],
+                        row[SecureDataTable.key],
+                        row[SecureDataTable.value])
+            }
         }
-
+        return entry
     }
-}
 
-fun deleteEntry(entry: SecureDataEntry) {
-    transaction {
-        deleteEntry(entry.type, entry.key)
+    fun getEntries(type: String): List<SecureDataEntry> {
+        val entries = ArrayList<SecureDataEntry>()
+        transaction {
+            addLogger(StdOutSqlLogger)
+
+            SecureDataTable.select { SecureDataTable.type eq type }.forEach { row ->
+                entries.add(SecureDataEntry(
+                        row[SecureDataTable.id].toString(),
+                        row[SecureDataTable.type],
+                        row[SecureDataTable.key],
+                        row[SecureDataTable.value]))
+            }
+
+        }
+        return entries
     }
-}
 
-fun deleteEntry(type: String, key: String) {
-    transaction {
-        addLogger(StdOutSqlLogger)
+    fun getAllEntries(): List<SecureDataEntry> {
+        val entries = ArrayList<SecureDataEntry>()
+        transaction {
+            SecureDataRow.all().forEach { entries.add(SecureDataEntry(it.id.toString(), it.type, it.key, it.value)) }
+        }
+        return entries
+    }
 
-        SecureDataTable.deleteWhere {
-            (SecureDataTable.type eq type) and (SecureDataTable.key eq key)
+    fun updateEntry(entry: SecureDataEntry) {
+        updateEntry(entry.type, entry.key, entry.value)
+    }
+
+    fun updateEntry(type: String, key: String, value: String?) {
+        transaction {
+            addLogger(StdOutSqlLogger)
+
+            SecureDataTable.update({ (SecureDataTable.type eq type) and (SecureDataTable.key eq key) })
+            {
+                it[SecureDataTable.value] = value
+            }
+
+        }
+    }
+
+    fun deleteEntry(entry: SecureDataEntry) {
+        transaction {
+            deleteEntry(entry.type, entry.key)
+        }
+    }
+
+    fun deleteEntry(type: String, key: String) {
+        transaction {
+            addLogger(StdOutSqlLogger)
+
+            SecureDataTable.deleteWhere {
+                (SecureDataTable.type eq type) and (SecureDataTable.key eq key)
+            }
+        }
+    }
+
+    fun deleteEntry(id: String) {
+        transaction {
+            addLogger(StdOutSqlLogger)
+
+            SecureDataTable.deleteWhere {
+                SecureDataTable.id eq UUID.fromString(id)
+            }
         }
     }
 }
-
-fun deleteEntry(id: String) {
-    transaction {
-        addLogger(StdOutSqlLogger)
-
-        SecureDataTable.deleteWhere {
-            SecureDataTable.id eq UUID.fromString(id)
-        }
-    }
-}
-
 
 object SecureDataTable : UUIDTable("secure_data") {
     val type = text("type")
