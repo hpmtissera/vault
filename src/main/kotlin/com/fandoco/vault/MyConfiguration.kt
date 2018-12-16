aa5ef698b1c45e7a2c8caf72e457cb71cea127fb
Prasad Tissera <hpmtissera@gmail.com>
2018-12-16 21:01:28 +0900

    add myconfig

---
+++ src/main/kotlin/com/fandoco/vault/MyConfiguration.kt
@@ -0,0 +1,21 @@
+package com.fandoco.vault
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+@Configuration
+class MyConfiguration {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurerAdapter() {
+            override fun addCorsMappings(registry: CorsRegistry?) {
+                registry!!.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+            }
+        }
+    }
+}
