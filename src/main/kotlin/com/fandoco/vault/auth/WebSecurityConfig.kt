ee16be3fbc607744418d9a12500d5cbbb29652d8
Prasad Tissera <hpmtissera@gmail.com>
2018-12-16 21:38:07 +0900

    add myconfig

--- src/main/kotlin/com/fandoco/vault/auth/WebSecurityConfig.kt
+++ src/main/kotlin/com/fandoco/vault/auth/WebSecurityConfig.kt
@@ -3,14 +3,12 @@ package com.fandoco.vault.auth
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-
 
 
 @Configuration
@@ -24,7 +22,6 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers("/", "/css/**/", "/js/**/").permitAll()
-                .antMatchers(HttpMethod.OPTIONS, "**").permitAll()
                 .anyRequest().authenticated()
                 .and().httpBasic()
                 .authenticationEntryPoint(authEntryPoint)
